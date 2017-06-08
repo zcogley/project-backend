c00e861d7983561d1c1c88d1bf08b1ee300195fe
Zac Cogley <zaccogley@gmail.com>
2017-06-07 21:10:37 -0500

    email configured

--- oggi/oggi/settings.py
+++ oggi/oggi/settings.py
@@ -115,6 +115,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend"
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request",
     "django.contrib.auth.context_processors.auth",
--- oggi/todo/models.py
+++ oggi/todo/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+import hashlib
 
 class item(models.Model):
     author = models.ForeignKey('auth.User')
@@ -7,3 +11,30 @@ class item(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+    def profile_image_url(self):
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        # return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
