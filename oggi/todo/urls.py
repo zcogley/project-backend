311e82f4af0906cf3f26562d927bb656331ade7c
Zac Cogley <zaccogley@gmail.com>
2017-06-11 15:05:46 -0500

    redirect root to index

--- oggi/todo/urls.py
+++ oggi/todo/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
     url(r'^today/$', views.today, name='today'),
     url(r'^move/$', views.move, name='move'),
